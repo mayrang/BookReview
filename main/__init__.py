f2f9d709b1913363a71fd5cd0d56fee328a9c813
Mayrang <pgss0626@gmail.com>
2022-03-17 20:30:19 +0900

    BookReview site first commit

---
+++ main/__init__.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from flask_pymongo import PyMongo
+
+app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/BookReview"
+app.config["SECRET_KEY"] = "elkajtlkajdlknatylk"
+
+
+mongo = PyMongo(app)
+
