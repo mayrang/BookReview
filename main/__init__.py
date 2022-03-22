05d30e70b8449a5192e48436ae51ba3b4e232ac1
mayrang <pkss0626@naver.com>
2022-03-22 23:01:55 +0900

    add_config_and_common.py_function

--- main/__init__.py
+++ main/__init__.py
@@ -3,11 +3,23 @@ from flask import Flask
 from flask_pymongo import PyMongo
 from flask import redirect, url_for, request, render_template, flash
 from datetime import timedelta
+import os
 
 app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/BookReview"
 app.config["SECRET_KEY"] = "elkajtlkajdlknatylk"
 app.config["PERMANENT_SESSION_LIFETIME"] = timedelta(minutes=30)
+
+ALLOWED_EXTENTIONS = set(["png", "jpeg", "jpg"])
+BOARD_IMAGE_PATH = "C:\\bookreview\\images"
+
+app.config["ALLOWED_EXTENTIONS"] = ALLOWED_EXTENTIONS
+app.config["BOARD_IMAGE_PATH"] = BOARD_IMAGE_PATH
+app.config["MAX_CONTENT_LENGTH"] = 15 * 1024 * 1024
+
+if not os.path.exists(app.config["BOARD_IMAGE_PATH"]):
+    os.mkdir(app.config["BOARD_IMAGE_PATH"])
+
 
 mongo = PyMongo(app)
 
--- main/common.py
+++ main/common.py
@@ -7,3 +7,13 @@ def check_password(hash_password, password):
 
 def make_hash_password(password):
     return generate_password_hash(password)
+
+
+def allowed_file(filename):
+    pass
+
+def random_generator(len=8):
+    pass
+
+def get_extension(filename):
+    pass
